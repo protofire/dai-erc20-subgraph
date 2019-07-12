16a75be894ecc234150104c79d02d58017ee1bc8
nicosampler <nf.dominguez.87@gmail.com>
2019-07-12 15:44:40 -0300

    inital commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,85 +1,31 @@
+import { BigDecimal, BigInt, Address } from "@graphprotocol/graph-ts";
 import {
-  Mint as MintEvent,
-  Burn as BurnEvent,
-  LogSetAuthority as LogSetAuthorityEvent,
-  LogSetOwner as LogSetOwnerEvent,
-  LogNote as LogNoteEvent,
-  Approval as ApprovalEvent,
-  Transfer as TransferEvent
-} from "../generated/Contract/Contract"
-import {
-  Mint,
-  Burn,
-  LogSetAuthority,
-  LogSetOwner,
-  LogNote,
-  Approval,
-  Transfer
-} from "../generated/schema"
+  Transfer as TransferEvent,
+  Contract as Dai
+} from "../generated/Contract/Contract";
+import { Transfer, Account } from "../generated/schema";
 
-export function handleMint(event: MintEvent): void {
-  let entity = new Mint(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.guy = event.params.guy
-  entity.wad = event.params.wad
-  entity.save()
+function getDaiInstance(address: Address): Dai {
+  return Dai.bind(address);
 }
 
-export function handleBurn(event: BurnEvent): void {
-  let entity = new Burn(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.guy = event.params.guy
-  entity.wad = event.params.wad
-  entity.save()
-}
-
-export function handleLogSetAuthority(event: LogSetAuthorityEvent): void {
-  let entity = new LogSetAuthority(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.authority = event.params.authority
-  entity.save()
-}
-
-export function handleLogSetOwner(event: LogSetOwnerEvent): void {
-  let entity = new LogSetOwner(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.save()
-}
-
-export function handleLogNote(event: LogNoteEvent): void {
-  let entity = new LogNote(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.sig = event.params.sig
-  entity.guy = event.params.guy
-  entity.foo = event.params.foo
-  entity.bar = event.params.bar
-  entity.wad = event.params.wad
-  entity.fax = event.params.fax
-  entity.save()
-}
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.src = event.params.src
-  entity.guy = event.params.guy
-  entity.wad = event.params.wad
-  entity.save()
+function castToDecimal(value: BigInt, decimalsCount: BigInt): BigDecimal {
+  return value.divDecimal(decimalsCount.toBigDecimal());
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
+  let dai = getDaiInstance(event.address);
+
+  let transfer = new Transfer(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.src = event.params.src
-  entity.dst = event.params.dst
-  entity.wad = event.params.wad
-  entity.save()
+  );
+  transfer.src = event.params.src;
+  transfer.account = event.params.src.toHex();
+  transfer.dst = event.params.dst;
+  transfer.value = castToDecimal(event.params.wad, dai.decimals());
+  transfer.save();
+
+
+  let account = new Account(transfer.account);
+  account.save();
 }
